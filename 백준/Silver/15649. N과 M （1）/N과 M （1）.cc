f97b4cd230af43b9790e21817d1a4684863c6914
손예진 <76722032+handyejin@users.noreply.github.com>
2024-04-25 23:00:30 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,25 @@
+#include<cstdio>
+int n,m;
+int arr[10];
+bool check[10];
+void pick(int cnt) {
+    if(cnt==m) {
+        for(int i=0; i<m; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+        return;
+    }
+    for(int i=1; i<=n; i++) {
+        if(!check[i]){
+            check[i]=true;
+            arr[cnt]=i;
+            pick(cnt+1);
+            check[i]=false;
+        }
+    }
+}
+int main(void){
+    scanf("%d %d", &n, &m);
+    pick(0);
+    return 0;
+}
