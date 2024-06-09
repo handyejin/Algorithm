53966df7a024a2c8f7e37354c19f27f91b8fff04
손예진 <76722032+handyejin@users.noreply.github.com>
2024-06-09 16:47:32 +0900

    [Gold V] Title: 암호 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,46 +1,23 @@
 import sys
 
 
-def is_valid_answer(answer):
-    vowel_count = 0
-    consonant_count = 0
-
-    for s in answer:
-        if s in ('a', 'e', 'o', 'u', 'i'):
-            vowel_count += 1
-        else:
-            consonant_count += 1
-    if vowel_count >= 1 and consonant_count >= 2:
-        return True
-    else:
-        return False
-
-
-def backtracking(depth, start):
-    if depth == l:
-        answer.sort()
-        s = ''.join(answer)
-        if s not in result:
-            if is_valid_answer(s):
-                result.add(s)
-                print(s)
+def backtracking(depth, cnt, answer):
+    if depth == c:
+        if len(answer) == l and cnt >= 1 and len(answer) - cnt >= 2:
+            print(''.join(answer))
         return
 
-    for i in range(start, c):
-        if not visited[i]:
-            visited[i] = True
-            answer.append(arr[i])
-            backtracking(depth + 1, i)
-            del answer[-1]
-            visited[i] = False
+    backtracking(depth + 1, cnt + tbl[ord(arr[depth])], answer + arr[depth])
+    backtracking(depth + 1, cnt, answer)
 
 
 l, c = map(int, sys.stdin.readline().strip().split())
 arr = list(sys.stdin.readline().strip().split())
-visited = [False] * c
-answer = []
-result = set()
-
 arr.sort()
+tbl = [0] * 128
 
-backtracking(0, 0)
+for ch in ('a', 'e', 'i', 'o', 'u'):
+    tbl[ord(ch)] = 1
+
+# depth, 모음의 개수, 완성되는 문자열
+backtracking(0, 0, '')
