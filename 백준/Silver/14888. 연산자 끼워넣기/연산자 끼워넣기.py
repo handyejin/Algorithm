15df27323f5cb26771fb04366be65f264ee595b5
손예진 <76722032+handyejin@users.noreply.github.com>
2024-06-09 21:24:39 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 7156 ms, Memory: 34708 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -5,13 +5,13 @@ def calculator(operator):
     s = n_arr[0]
 
     for i in range(n - 1):
-        if operator[i] == 0:
+        if operator[i] == '0':
             s = s + n_arr[i + 1]
-        elif operator[i] == 1:
+        elif operator[i] == '1':
             s = s - n_arr[i + 1]
-        elif operator[i] == 2:
+        elif operator[i] == '2':
             s = s * n_arr[i + 1]
-        elif operator[i] == 3:
+        elif operator[i] == '3':
             s = int(s / n_arr[i + 1])
     return s
 
@@ -19,20 +19,22 @@ def calculator(operator):
 def backtracking(depth, answer):
     global visited, max_num, min_num
     if depth == n - 1:
-        s = calculator(answer)
-        if s > max_num:
-            max_num = s
-        if s < min_num:
-            min_num = s
-        return
+        if answer not in answer_set:
+            s = calculator(answer)
+            if s > max_num:
+                max_num = s
+            if s < min_num:
+                min_num = s
+            answer_set.add(answer)
+            return
 
     for idx in range(0, n - 1):
         if not visited[idx]:
             visited[idx] = True
-            answer.append(p_operator[idx])
+            answer += str(p_operator[idx])
             backtracking(depth + 1, answer)
             visited[idx] = False
-            del answer[-1]
+            answer = answer[0:-1]
 
 
 n = int(sys.stdin.readline())
@@ -45,9 +47,9 @@ for i in range(4):
     p_operator.extend(opt)
 
 visited = [False] * (n - 1)
-
+answer_set = set()
 min_num, max_num = 1000000000, -1000000000
 
-backtracking(0, [])
+backtracking(0, '')
 print(max_num)
 print(min_num)
