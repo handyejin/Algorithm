b513140c05e8af1a76192a7d12510761cb18ab28
손예진 <76722032+handyejin@users.noreply.github.com>
2024-06-26 21:24:44 +0900

    [Silver IV] Title: 카드 놓기, Time: 1404 ms, Memory: 116556 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def dfs(depth):
+    if depth == k:
+        anw = ''.join(str(x) for x in result)
+        if anw not in result_set:
+            result_set.add(anw)
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            result.append(arr[i])
+            dfs(depth + 1)
+            visited[i] = False
+            del result[-1]
+
+
+n = int(sys.stdin.readline().strip())
+k = int(sys.stdin.readline().strip())
+arr = list()
+result_set = set()
+result = []
+visited = [False] * n
+
+for _ in range(n):
+    arr.append(int(sys.stdin.readline().strip()))
+
+dfs(0)
+print(len(result_set))
